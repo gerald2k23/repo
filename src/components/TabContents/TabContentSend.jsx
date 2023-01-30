87cbb2495b995cd5a8e5fa00c2ea33704a7a525c
rromikas <rromikas@gmail.com>
2023-01-31 00:22:41 +0200

    added loading spinner and message upon successfull transfer

---
+++ src/components/TabContents/TabContentSend.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import {
+  Button,
+  InputAmountContainer,
+  TabContent,
+  TabContentSelectAsset,
+  TabFooter,
+  TableInputSend,
+  TransactionForm,
+} from '..';
+import { SelectAssetContext } from '../../contexts/SelectAssetContext';
+import { convertToCurrency } from '../../utilities/convert-to-currency';
+import { useContext } from 'react';
+import { ReactComponent as Spinner } from '../../assets/spinner.svg';
+import './TabContentSend.css';
+
+const TabContentSend = () => {
+  const { isSelectAssetOpen, selectedCoin } = useContext(SelectAssetContext);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState();
+
+  const onSubmit = (e, defaultSubmit) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      defaultSubmit();
+      setMessage('Transfer Completed');
+      setLoading(false);
+    }, 4000);
+  };
+
+  return (
+    <>
+      {isSelectAssetOpen ? (
+        <TabContentSelectAsset />
+      ) : (
+        <TabContent>
+          <TransactionForm onSubmit={onSubmit} type='sendCoin'>
+            <div className='relative'>
+              <div className={(loading || message) && 'opacity-0'}>
+                <InputAmountContainer />
+              </div>
+
+              <div className='absolute left-0 top-0 w-full flex justify-center'>
+                {!loading ? null : (
+                  <Spinner className='spinner' width={80} height={80}></Spinner>
+                )}
+                {!message ? null : (
+                  <div>
+                    <div className='text-3xl text-brand leading-[70px] font-bold'>
+                      {message}
+                    </div>
+                    <div
+                      onClick={() => setMessage(undefined)}
+                      className='opacity-50 text-center cursor-pointer hover:opacity-80'>
+                      Close message
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+            <TableInputSend />
+            <Button size='xl'>{`Send ${selectedCoin?.name}`}</Button>
+          </TransactionForm>
+          <TabFooter
+            textLeft={`${selectedCoin?.symbol} balance`}
+            textRight={`${selectedCoin?.balance_coin?.toFixed(6)} ${
+              selectedCoin?.symbol
+            } = ${convertToCurrency(selectedCoin?.balance_eur)}`}
+          />
+        </TabContent>
+      )}
+    </>
+  );
+};
+
+export default TabContentSend;

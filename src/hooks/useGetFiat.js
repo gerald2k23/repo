4924ba8277ff02613a401f6c5bc272270838f09c
rromikas <rromikas@gmail.com>
2023-01-31 21:10:00 +0200

    currency update

--- src/hooks/useGetFiat.js
+++ src/hooks/useGetFiat.js
@@ -10,9 +10,18 @@ const useGetFiat = () => {
     const yourFiatQuery = query(collection(db, 'yourFiat'));
 
     return onSnapshot(yourFiatQuery, (querySnapshot) => {
-      setFiat(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setFiat(
+        querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          symbol: 'USD',
+          name: 'Dollar',
+          icon: 'https://firebasestorage.googleapis.com/v0/b/fireck-4c36e.appspot.com/o/ccm8H3qiABeVqHFGst1b4Y.svg?alt=media&token=e807e17a-0522-44f7-b00d-b40a9e0a4b5b',
+          id: doc.id,
+        }))
+      );
     });
   };
+  console.log({ fiat });
 
   useEffect(() => {
     fetchYourFiat(db);
